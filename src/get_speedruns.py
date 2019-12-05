67423a690c741e5439b3174a0bdde3b28e6e6aa3
cjuracek <therealjuracek@gmail.com>
2019-12-05 10:30:55 -0500

    Utilized speedrun.com's API to access data. Got world record date for 1 category of 1 game.

--- src/get_speedruns.py
+++ src/get_speedruns.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
-url = "https://www.speedrun.com/games"
+url = "https://www.speedrun.com/api/v1/leaderboards/o1y9wo6q/category/7dgrrxk4?top=1"
 r = requests.get(url)
-soup = BeautifulSoup(r.content, "html.parser")
+game_info = json.loads(r.text)
+wr_date = game_info.get("data").get("runs")[0].get("run").get("date")
