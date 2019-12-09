5dceba0bdce2900ce7fbff15b35f4c1e8648cac2
cjuracek <therealjuracek@gmail.com>
2019-12-09 10:09:17 -0500

    Added data structure to hold full names of games for printing

--- src/get_speedruns.py
+++ src/get_speedruns.py
@@ -3,10 +3,17 @@ import requests
 import json
 
 
-games = {"sm64": "120_Star",
-         "smo": "Any",
-         "sms": "120_Shines"}#, "z27o9gd0"}
+game_categories = {"sm64": "120_Star",
+                   "smo": "Any",
+                   "sms": "120_Shines",
+                   "smb1": "Any",
+                   "botw": "Any"}
 
+game_names = {"sm64": "Super Mario 64",
+              "smo": "Super Mario Odyssey",
+              "sms": "Super Mario Sunshine",
+              "smb1": "Super Mario Bros.",
+              "botw": "Breath of the Wild"}
 
 def is_new_wr(wr_date):
     past = datetime.today() - timedelta(weeks=1)
@@ -21,7 +28,7 @@ if __name__ == '__main__':
 
     # Example URL: "https://www.speedrun.com/api/v1/leaderboards/o1y9wo6q/category/7dgrrxk4?top=1"
     base_url = "https://www.speedrun.com/api/v1/leaderboards/"
-    for game, category in games.items():
+    for game, category in game_categories.items():
         url = f'{base_url}{game}/category/{category}?top=1'
         r = requests.get(url)
         game_info = json.loads(r.text)
@@ -32,4 +39,4 @@ if __name__ == '__main__':
             print(f'NEW WORLD RECORD FOR {game} ON {wr_date_str}')
         else:
             time_diff = datetime.today() - wr_date
-            print(f'{game} : {category} No new world records since {wr_date_str} ({time_diff.days} Days)')
+            print(f'{game_names.get(game)}: {category} No new world records since {wr_date_str} ({time_diff.days} Days)')
