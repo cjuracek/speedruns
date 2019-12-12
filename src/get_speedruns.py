2ebacc7d3d183362ebaa680768f4f7f55131e656
cjuracek <therealjuracek@gmail.com>
2019-12-12 09:15:33 -0500

    Created function to get full game name from abbreviation, eliminating need for dictionary of names

--- src/get_speedruns.py
+++ src/get_speedruns.py
@@ -17,18 +17,6 @@ game_categories = {'sm64': ['120_Star', '70_Star', '16_Star', '1_Star', '0_Star'
                    'celeste': ['Any'],
                    'smb3': ['100']}
 
-game_names = {'sm64': 'Super Mario 64',
-              'smo': 'Super Mario Odyssey',
-              'sms': 'Super Mario Sunshine',
-              'smb1': 'Super Mario Bros.',
-              'botw': 'Breath of the Wild',
-              'supermonkeyball': 'Super Monkey Ball',
-              'supermonkeyball2': 'Super Monkey Ball 2',
-              'twwhd': 'Wind Waker HD',
-              'oot': 'Ocarina of Time',
-              'celeste': 'Celeste',
-              'smb3': 'Super Mario Bros. 3'}
-
 
 def is_new_wr(wr_date):
     past = datetime.today() - timedelta(weeks=1)
@@ -39,26 +27,35 @@ def get_wr_date(json):
     return json.get('data').get('runs')[0].get('run').get('date')
 
 
+def get_full_game_name(abbreviation):
+    url = f"https://www.speedrun.com/api/v1/games/{abbreviation}"
+    r = requests.get(url)
+    game_info = json.loads(r.text)
+    game_name = game_info.get('data').get('names').get('international')
+    return(game_name)
+
 if __name__ == '__main__':
 
     # Example URL: 'https://www.speedrun.com/api/v1/leaderboards/o1y9wo6q/category/7dgrrxk4?top=1'
     base_url = 'https://www.speedrun.com/api/v1/leaderboards/'
     for game, categories in game_categories.items():
+        game_name = get_full_game_name(game)
         for category in categories:
             url = f'{base_url}{game}/category/{category}?top=1'
+            print()
             r = requests.get(url)
             wr_info = json.loads(r.text)
             wr_date_str = get_wr_date(wr_info)
             wr_date = datetime.strptime(wr_date_str, '%Y-%m-%d')
 
             if is_new_wr(wr_date):
-                print(f'NEW WORLD RECORD FOR {game} ON {wr_date_str}')
+                print(f'NEW WORLD RECORD FOR {game_name} ON {wr_date_str}')
             else:
                 time_diff = datetime.today() - wr_date
-                print(f'{game_names.get(game)}: {category} No new world records since '
+                print(f'{game_name}: {category} No new world records since '
                       f'{wr_date_str} ({time_diff.days} Days)')
 
-            time.sleep(1)
+            time.sleep(2)
 
     # Send email
     # https://realpython.com/python-send-email/
