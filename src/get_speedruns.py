bc045733a7dd6c442d134f7e8177f9b692d51348
cjuracek <therealjuracek@gmail.com>
2019-12-17 15:22:55 -0500

    Created email message + completed sending

--- src/get_speedruns.py
+++ src/get_speedruns.py
@@ -6,17 +6,19 @@ import time
 import smtplib, ssl
 
 
-game_categories = {'sm64': ['120_Star', '70_Star', '16_Star', '1_Star', '0_Star'],
-                   'smo': ['Any'],
-                   'sms': ['120_Shines', 'Any'],
-                   'smb1': ['Any'],
-                   'botw': ['Any'],
-                   'supermonkeyball': ['Expert'],
-                   'supermonkeyball2': ['Story_Mode'],
-                   'twwhd': ['Any'],
-                   'oot': ['Any'],
-                   'celeste': ['Any'],
-                   'smb3': ['100']}
+game_categories = {'Super Mario 64': ['120_Star', '70_Star', '16_Star', '1_Star', '0_Star'],
+                   'Super Mario Odyssey': ['Any'],
+                   'Super Mario Sunshine': ['120_Shines', 'Any'],
+                   'Super Mario Bros.': ['Any', 'Warpless'],
+                   'The Legend of Zelda: Breath of the Wild': ['Any'],
+                   'Super Monkey Ball': ['Expert'],
+                   'Super Monkey Ball 2': ['Story_Mode'],
+                   'The Legend of Zelda: The Wind Waker HD': ['Any'],
+                   'The Legend of Zelda: Ocarina of Time': ['Any'],
+                   'Celeste': ['Any'],
+                   'Super Mario Bros. 3': ['100'],
+                   'Castlevania (NES)': ['Any'],
+                   'Super Meat Boy': ['Any']}
 
 
 def is_new_wr(wr_date):
@@ -38,30 +40,35 @@ def get_full_game_name(abbreviation):
 def get_game_id(game_name):
     game_name_url = game_name.replace(" ", "%20")
     url = f"https://www.speedrun.com/api/v1/games?name={game_name_url}"
+    r = requests.get(url)
+    game_info = json.loads(r.text)
+    game_id = game_info.get('data')[0].get('id')
+    return game_id
 
 
 if __name__ == '__main__':
 
     # Example URL: 'https://www.speedrun.com/api/v1/leaderboards/o1y9wo6q/category/7dgrrxk4?top=1'
     base_url = 'https://www.speedrun.com/api/v1/leaderboards/'
+    all_wr_info = []
     for game, categories in game_categories.items():
         game_id = get_game_id(game)
         for category in categories:
             url = f'{base_url}{game_id}/category/{category}?top=1'
-            print()
             r = requests.get(url)
             wr_info = json.loads(r.text)
             wr_date_str = get_wr_date(wr_info)
             wr_date = datetime.strptime(wr_date_str, '%Y-%m-%d')
 
             if is_new_wr(wr_date):
-                print(f'NEW WORLD RECORD FOR {game} ON {wr_date_str}')
+                wr_message = f'NEW WORLD RECORD FOR {game} ON {wr_date_str}'
+                all_wr_info.append(wr_message)
             else:
                 time_diff = datetime.today() - wr_date
-                print(f'{game}: {category} No new world records since '
-                      f'{wr_date_str} ({time_diff.days} Days)')
+                wr_message = f'{game}: {category} - No new world records since 'f'{wr_date_str} ({time_diff.days} Days)'
+                all_wr_info.append(wr_message)
 
-            time.sleep(2)
+            time.sleep(1)
 
     # Send email
     # https://realpython.com/python-send-email/
@@ -73,7 +80,7 @@ if __name__ == '__main__':
 
     email = 'cjuracektest@gmail.com'
     subject = 'Speed Runs'
-    body = 'This is a Python message'
+    body = '\n'.join(all_wr_info)
     message = f'Subject: {subject} \n\n {body}'
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(email, password)
